93a9535fa9e771943c5f6f407db0520e637bdd76
necrotic7 <necrotic2971@gmail.com>
2018-12-17 21:02:56 +0800

    lesson1217

--- books/admin.py
+++ books/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
+
+@admin.register(Book) ##等同於->admin.site.register(Book,BookAdmin)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['name','price']
+    search_fields = ['name']
+    list_filter = ['price']
+
+
--- books/views.py
+++ books/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Book
+# from django.http import Http404
 
-# Create your views here.
+def index(request):
+    books = Book.objects.all()
+    return render(request,'books/index.html',{'books':books})
+
+def show(request,pk):
+    # try:
+    #     book = Book.objects.get(pk=pk)
+    # except:
+    #     raise Http404
+    book=get_object_or_404(Book,pk=pk)
+    return render(request,'books/show.html',{'book':book})
+
+def add(request):
+    print(request.method)
+    if request.method == 'POST':
+        print(request.POST)
+    return render(request,'books/add.html')
